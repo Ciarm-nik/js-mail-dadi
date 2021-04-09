f16134e525906c4b91cb44e81b3b11a9759c06d3
Ciarm-nik <nikcia@libero.it>
2021-04-09 18:09:52 +0200

    Ciclo for inserito

--- Mail/my_script.js
+++ Mail/my_script.js
@@ -1,8 +1,23 @@
 var inserisciMail = prompt ("Inserisci la tua mail");
 
 //Creo una lista (array) delle mail
-var listaEmail = ["ciao@libero.it", "verde@libero.it", "rosso@libero.it", "blu@libero.it", "nero@libero.it", "bianco@libero.it", "nero1@libero.it", "verde2@libero.it", "rosso1@libero.it",]
-console.log (listaEmail)
+var listaEmail = ["ciao@libero.it", "verde@libero.it", "rosso@libero.it", "blu@libero.it", "nero@libero.it", "bianco@libero.it", "nero1@libero.it", "verde2@libero.it", "rosso1@libero.it"];
+// console.log (listaEmail)
 
-//La tua mail è inserita correttamente
-//La tua mail non è corretta
+var emailVerificata = false;
+
+//Inizio ciclo per verificare l'email corretta
+for (var i = 0; i < listaEmail.length; i++) {
+    if (listaEmail[i] == inserisciMail) {
+        var emailVerificata = true;
+    } 
+}
+//email corretta
+if (emailVerificata == true) {
+    console.log("La tua mail è presente nella lista...benvenuto nel sito")
+//email errata    
+} else {
+    alert("La tua email non è corretta...riprova")
+}
+
+
