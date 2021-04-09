c7e1c2138348c87d3a85c65f4e6a2beb7d62f622
Ciarm-nik <nikcia@libero.it>
2021-04-09 15:30:37 +0200

    Inizio esercizio mail

---
+++ Mail/my_script.js
@@ -0,0 +1,8 @@
+var inserisciMail = prompt ("Inserisci la tua mail");
+
+//Creo una lista (array) delle mail
+var listaEmail = ["ciao@libero.it", "verde@libero.it", "rosso@libero.it", "blu@libero.it", "nero@libero.it", "bianco@libero.it", "nero1@libero.it", "verde2@libero.it", "rosso1@libero.it",]
+console.log (listaEmail)
+
+//La tua mail è inserita correttamente
+//La tua mail non è corretta
