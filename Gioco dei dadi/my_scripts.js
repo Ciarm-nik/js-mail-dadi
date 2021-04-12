4d71b490fb8d942dd3226810be8d8bfe8a70f4fe
Ciarm-nik <nikcia@libero.it>
2021-04-12 09:22:43 +0200

    Modifica indentazione

--- Gioco dei dadi/my_scripts.js
+++ Gioco dei dadi/my_scripts.js
@@ -1,18 +1,20 @@
 // Il giocatore lancia un dado
-var primoLancio = Math.floor(Math.random() * 6); 
+var primoLancio = Math.floor(Math.random() * 6)  +1; 
 console.log ("Il punteggio del giocatore è " + primoLancio);
 
 // Il computer lancia un dado
-var secondoLancio = Math.floor(Math.random() * 6); 
+var secondoLancio = Math.floor(Math.random() * 6) +1; 
 console.log ("Il punteggio del giocatore è " + secondoLancio);
 
-
+// In questo caso vince il giocatore
 if (primoLancio > secondoLancio) {
     console.log("Giocatore vince!!");
-} 
+}
+// In questo caso vince il computer
 else if (primoLancio < secondoLancio) {
     console.log("Computer vince!!");
-} 
+}
+// Nessun vincitore 
 else if (primoLancio == secondoLancio) {
     console.log("Nessun vincitore!!");
 }
